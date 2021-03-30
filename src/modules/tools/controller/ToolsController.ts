ce6f54f1fc3588ef3207d22ddd86f072c8f9bea5
Isabelar07 <isabelarochas@outlook.com.br>
2021-03-30 19:14:14 -0300

    feat: criar requisição de obter ferramenta por tag

--- src/modules/tools/controller/ToolsController.ts
+++ src/modules/tools/controller/ToolsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { IdGenerator } from "../../../shared/infra/services/IdGenerator";
 import { TagBusiness } from '../../tags/business/TagBusiness';
 import { TagDataBase } from '../../tags/data/TagDataBase';
+import { ITagOutputQueryDTO } from '../../tags/interfaces/Tag';
 import { ToolsBusiness } from "../business/ToolsBusiness";
 import { ToolsDataBase } from "../data/ToolsDataBase";
 import { IToolInputDTO, IToolOutputDTO } from '../interfaces/Tools';
@@ -54,4 +55,23 @@ export class ToolsController {
             return res.status(500).send({ error: error.message });
         }
     }
+
+    // async getByTag(req: Request, res: Response) {
+
+    //     try {
+
+    //         const { tag } = req.query 
+
+    //         const tags = await toolsBusiness.getByTag({tag})
+
+    //         // const output: ITagOutputQueryDTO {
+    //         //     name: tags.tag.map(t => t.name)
+    //         // }
+
+    //         return res.status(200).send(tags)
+
+    //     } catch (error) {
+    //         return res.status(500).send({ error: error.message });
+    //     }
+    // }
 }
